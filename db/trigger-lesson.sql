31f8479921e8b3f98ec5b515d2100367703f25a1
zenit1925 <kostia-85-85@yandex.ru>
2024-04-05 18:05:52 +0300

    2. Триггеры [#504803] - Исправления

--- db/trigger-lesson.sql
+++ db/trigger-lesson.sql
@@ -9,9 +9,12 @@ create table products
 
 CREATE OR REPLACE FUNCTION calculate_tax()
     RETURNS TRIGGER AS $$
+DECLARE
+    temp_price INTEGER;
 BEGIN
-    NEW.price := NEW.price + (NEW.price * 0.1); -- Добавляем налог в размере 10%
-    RETURN NEW;
+    UPDATE products
+    SET price = price + (price * 0.1);
+    RETURN NULL;
 END;
     $$ LANGUAGE plpgsql;
 
@@ -32,3 +35,27 @@ CREATE TRIGGER before_insert_tax_trigger
     BEFORE INSERT ON products
     FOR EACH ROW
 EXECUTE FUNCTION calculate_tax_row();
+
+-- Пункт №3 задачи
+
+CREATE TABLE history_of_price
+(
+    id    SERIAL PRIMARY KEY,
+    name  VARCHAR(50),
+    price INTEGER,
+    date  TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION insert_price_history()
+    RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO history_of_price (name, price, date)
+    VALUES (NEW.name, NEW.price, NOW());
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER after_insert_price_trigger
+AFTER INSERT ON products
+FOR EACH ROW
+EXECUTE FUNCTION insert_price_history();
