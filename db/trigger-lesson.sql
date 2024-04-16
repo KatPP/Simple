1339a7ebd494cbc54bb33e80ea64a0833186effd
zenit1925 <kostia-85-85@yandex.ru>
2024-04-16 14:01:45 +0300

    2. Триггеры [#504803] - Исправления 3

--- db/trigger-lesson.sql
+++ db/trigger-lesson.sql
@@ -1,61 +1,52 @@
-create table products
-(
-    id       serial primary key,
-    name     varchar(50),
-    producer varchar(50),
-    count    integer default 0,
-    price    integer
-);
-
-CREATE OR REPLACE FUNCTION calculate_tax()
+-- Триггер 1 (Уровень оператора)
+CREATE OR REPLACE FUNCTION calculate_tax_statement()
     RETURNS TRIGGER AS $$
-DECLARE
-    temp_price INTEGER;
 BEGIN
     UPDATE products
-    SET price = price + (price * 0.1);
-    RETURN NULL;
+    SET price = price * 1.1; -- Добавление 10% налога
+    RETURN NEW;
 END;
     $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER after_insert_tax_trigger
+CREATE TRIGGER trigger_statement_tax
 AFTER INSERT ON products
 FOR EACH STATEMENT
-EXECUTE FUNCTION calculate_tax();
+EXECUTE FUNCTION calculate_tax_statement();
 
+-- Триггер 2 (Уровень строки)
 CREATE OR REPLACE FUNCTION calculate_tax_row()
 RETURNS TRIGGER AS $$
 BEGIN
-NEW.price := NEW.price + (NEW.price * 0.1); -- Добавляем налог в размере 10%
+NEW.price = NEW.price * 1.1; -- Добавление 10% налога
 RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER before_insert_tax_trigger
+CREATE TRIGGER trigger_row_tax
     BEFORE INSERT ON products
     FOR EACH ROW
 EXECUTE FUNCTION calculate_tax_row();
 
--- Пункт №3 задачи
-
+-- Создание таблицы history_of_price
 CREATE TABLE history_of_price
 (
-    id    SERIAL PRIMARY KEY,
-    name  VARCHAR(50),
-    price INTEGER,
-    date  TIMESTAMP
+    id    serial PRIMARY KEY,
+    name  varchar(50),
+    price integer,
+    date  timestamp
 );
 
-CREATE OR REPLACE FUNCTION insert_price_history()
+-- Триггер для вставки в таблицу history_of_price
+CREATE OR REPLACE FUNCTION insert_history_of_price()
     RETURNS TRIGGER AS $$
 BEGIN
     INSERT INTO history_of_price (name, price, date)
-    VALUES (NEW.name, NEW.price, NOW());
-    RETURN NULL;
+    VALUES (NEW.name, NEW.price, CURRENT_TIMESTAMP);
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER after_insert_price_trigger
+CREATE TRIGGER trigger_insert_history
 AFTER INSERT ON products
 FOR EACH ROW
-EXECUTE FUNCTION insert_price_history();
+EXECUTE FUNCTION insert_history_of_price();
